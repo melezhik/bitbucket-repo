e3810d631a802d33762903ad566689ecbfa2dac2
Alexey Melezhik <melezhik@gmail.com>
2016-11-01 13:02:16 +0300

    sleep and chunk

--- story.bash
+++ story.bash
@@ -8,6 +8,8 @@ export project=$(config project)
 
 export repo=$(config repo)
 export postfix=$(config postfix)
+export chunk=$(config chunk)
+export sleep=$(config sleep)
 
 echo svn_repo       : $svn_repo
 echo bitbucket user : $user
@@ -15,20 +17,26 @@ echo team           : $team
 echo project        : $project
 echo repo           : $repo
 echo postfix        : $postfix
+echo chunk          : $chunk
 
 export q="'";
 
 mkdir -p ~/bitbucket-repo/cache
+rm -rf  ~/bitbucket-repo/lock
+mkdir -p ~/bitbucket-repo/lock
 
 svn list $svn_repo| grep '/' | perl  -n -e '
 
   BEGIN {
+    $lid = 0;
     $user = $ENV{user}; 
     $repo = $ENV{repo}; 
     $project = $ENV{project}; 
     $team = $ENV{team}; 
     $password = $ENV{password}; 
     $postfix = $ENV{postfix}; 
+    $chunk = $ENV{chunk}; 
+    $sleep = $ENV{sleep}; 
     $q = $ENV{q};
   }
 
@@ -40,6 +48,11 @@ svn list $svn_repo| grep '/' | perl  -n -e '
 
   $i++;
 
+  if ($i % $chunk == 0){
+    $lid++;
+    print "touch ~/bitbucket-repo/lock/lock.$lid\n";
+  }
+
   print " 
 
   ( \\
@@ -50,9 +63,10 @@ svn list $svn_repo| grep '/' | perl  -n -e '
     echo repo $team/$r$postfix already exists
     touch ~/bitbucket-repo/cache/$r$postfix
   else
+    sleep $sleep; flock ~/bitbucket-repo/lock/lock.$lid \\
     curl -s -k -H \"Content-Type: application/json\" \\
-    -w \" ... $pack/$i %{url_effective} %{http_code} \" -d $q { \"is_private\" :  true , \"project\" : { \"key\" : \"$project\" }  } $q \\
-    https://api.bitbucket.org/2.0/repositories/$team/$r$postfix -u $user:$password 
+    -w \" |chunk: $chunk| |nn: $i| %{url_effective} %{http_code} \" -d $q { \"is_private\" :  true , \"project\" : { \"key\" : \"$project\" }  } $q \\
+    https://api.bitbucket.org/2.0/repositories/$team/$r$postfix -u $user:$password ; echo
   fi \\
   ) & \n "' | bash && echo bitbucket-repo-done
 
